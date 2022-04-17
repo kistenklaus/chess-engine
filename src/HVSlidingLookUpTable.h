cf537d47cb0609b9ddfbeadebfecfd97597e38c6
kistenklaus <karlsasssie@gmail.com>
2022-04-17 19:28:04 +0200

    add HVSlidingLookUpTable
    all LookUpTables are not tested!!!!

---
+++ src/HVSlidingLookUpTable.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cassert>
+#include <iostream>
+
+#include "bitmap.h"
+#include "tile_index.h"
+
+class HVSlidingLookUpTable {
+ private:
+  bitmap_t psudo_legal_moves[64];
+  HVSlidingLookUpTable();
+
+ public:
+  HVSlidingLookUpTable(HVSlidingLookUpTable const&) = delete;
+  HVSlidingLookUpTable& operator=(HVSlidingLookUpTable const&) = delete;
+
+  bitmap_t operator[](tile_index_t tile_index) {
+    assert(tile_index >= 0);
+    assert(tile_index < 64);
+    return psudo_legal_moves[tile_index];
+  }
+
+  static HVSlidingLookUpTable& get();
+};
