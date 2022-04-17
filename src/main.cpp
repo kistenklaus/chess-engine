aa80cee78aa267c03a07d3eb257ab2df012f3f5a
kistenklaus <karlsasssie@gmail.com>
2022-04-17 18:46:19 +0200

    add KnightLookUpTable and compute it as Singelton

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 #include "Board.h"
+#include "KnightLookUpTable.h"
 #include "fen.h"
 
 int main() {
-  Board board = board_from_fen("");
+  Board board = board_from_fen("not yet implemented");
   std::cout << board << std::endl;
+
+  KnightLookUpTable::get()[10];
 }
