0e177dd2fd94f80652d4cb4e278319783f1880ad
kistenklaus <karlsasssie@gmail.com>
2022-04-18 11:11:50 +0200

    fix DSlidingLookUpTable

--- src/DSlidingLookUpTable.cpp
+++ src/DSlidingLookUpTable.cpp
@@ -3,34 +3,27 @@
 DSlidingLookUpTable::DSlidingLookUpTable() {
   for (int row = 0; row < 8; row++) {
     for (int column = 0; column < 8; column++) {
-      bitmap_t bitmap = 0x0;
+      bitmap_t lookUp = 0x0;
 
       // Slide top left
-      for (int rx = row + 1; rx < 7; rx++) {
-        for (int cx = column - 1; cx >= 0; cx--) {
-          bitmap |= ((bitmap_t)0x1ULL) << (rx * 8 + cx);
-        }
+      for (int rx = row + 1, cx = column - 1; rx < 8 && cx >= 0; rx++, cx--) {
+        lookUp |= ((bitmap_t)0x1ULL) << (rx * 8 + cx);
       }
       // Slide top right
-      for (int rx = row + 1; rx < 7; rx++) {
-        for (int cx = column + 1; cx < 7; cx++) {
-          bitmap |= ((bitmap_t)0x1ULL) << (rx * 8 + cx);
-        }
-      }
-      // Slide bottom right
-      for (int rx = row - 1; rx >= 0; rx--) {
-        for (int cx = column + 1; cx < 7; cx++) {
-          bitmap |= ((bitmap_t)0x1ULL) << (rx * 8 + cx);
-        }
-      }
-      // Slide bottom left
-      for (int rx = row - 1; rx >= 0; rx--) {
-        for (int cx = column - 1; cx >= 0; cx--) {
-          bitmap |= ((bitmap_t)0x1ULL) << (rx * 8 + cx);
-        }
+      for (int rx = row + 1, cx = column + 1; rx < 8 && cx < 8; rx++, cx++) {
+        lookUp |= ((bitmap_t)0x1ULL) << (rx * 8 + cx);
       }
 
-      psudo_legal_moves[row * 8 + column] = bitmap;
+      // Slide bottom right
+      for (int rx = row - 1, cx = column + 1; rx >= 0 && cx < 8; rx--, cx++) {
+        lookUp |= ((bitmap_t)0x1ULL) << (rx * 8 + cx);
+      }
+      // Slide bottom left
+      for (int rx = row - 1, cx = column - 1; rx >= 0 && cx >= 0; rx--, cx--) {
+        lookUp |= ((bitmap_t)0x1ULL) << (rx * 8 + cx);
+      }
+
+      psudo_legal_moves[row * 8 + column] = lookUp;
     }
   }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main() {
   std::cout << board << std::endl;
 
   for (int i = 0; i < 64; i++) {
-    bitmap_t lookUp = KingLookUpTable::get()[i];
+    bitmap_t lookUp = DSlidingLookUpTable::get()[i];
     std::cout << i << std::endl;
     std::cout << bitmap_to_bitboard_string(lookUp);
     std::cout << std::endl;
